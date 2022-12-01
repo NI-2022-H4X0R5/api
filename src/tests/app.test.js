3663bc811e0df88546f03a3c51b06d73aa558ef4
Xen0Xys <red57101@gmail.com>
2022-12-02 00:58:59 +0100

    :bug: Fix test for new route

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -4,7 +4,8 @@ const request = supertest(app)
 
 describe("Test /api/users/:id route", () => {
     it("should return a response", async () => {
-        const response = await request.get("/api/users/1")
+        const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2OTkzNzkzNywiZXhwIjoxNjcwMDI0MzM3fQ.9SVHcfa-wMrpT3FynJ9tM7VgCFZlaNJMRVTXfjfhA5M`;
+        const response = await request.get("/api/users/1").set('Content-type', 'application/json').set("Authorization", `Bearer ${token}`);
         expect(response.status).toBe(200)
     })
 })
