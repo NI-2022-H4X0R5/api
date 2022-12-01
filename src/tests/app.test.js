7c6b93864220155bcac4cd107c301d02aef04eff
Xen0Xys <red57101@gmail.com>
2022-12-02 00:50:21 +0100

    :test_tube: Add tests for new route added

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -2,10 +2,9 @@ const app = require("../app")
 const supertest = require("supertest")
 const request = supertest(app)
 
-describe("/test endpoint", () => {
+describe("Test /api/users/:id route", () => {
     it("should return a response", async () => {
-        const response = await request.get("/test")
+        const response = await request.get("/api/users/1")
         expect(response.status).toBe(200)
-        expect(response.text).toBe("Hello world");
     })
 })
