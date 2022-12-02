7ea0bc7e7ade7cc8ec0bce691e7b3ee12eb262c2
Xen0Xys <red57101@gmail.com>
2022-12-02 04:07:20 +0100

    :rocket: Add more questions

--- src/resources/mockQuestions.js
+++ src/resources/mockQuestions.js
@@ -1,8 +1,9 @@
 module.exports = [
     {
         "name": "mst",
-        "type": "radio",
+        "type": "question",
         "ageRange": "adulte",
+        "content": "Qu'est ce qu'une MST ?",
         "hintLabel": "Aide",
         "hintContent": "Une MST est une maladie sexuellement transmissible",
         "responses": [
@@ -16,6 +17,206 @@ module.exports = [
             },
             {
                 "content": "Jm'en branle, c'est de bonne augure",
+                "score": 0
+            }
+        ]
+    },
+    {
+        "name": "ist",
+        "type": "question",
+        "ageRange": "adulte",
+        "content": "Qu'est ce qu'une IST ?",
+        "hintLabel": "Aide",
+        "hintContent": "Une IST est une infection sexuellement transmissible",
+        "responses": [
+            {
+                "content": "Infection sexuellement transmissibles",
+                "score": 1
+            },
+            {
+                "content": "Institut supérieur de technologie",
+                "score": 0
+            },
+            {
+                "content": "Inspirer, Soupirer, Temporiser",
+                "score": 0
+            },
+            {
+                "content": "Is Siri Trustworthy ?",
+                "score": 0
+            }
+        ]
+    },
+    {
+        "name": "protect_from_ist",
+        "type": "question",
+        "ageRange": "adulte",
+        "content": "Comment se protéger des IST ?",
+        "hintLabel": "Aide",
+        "hintContent": "",
+        "responses": [
+            {
+                "content": "Utiliser des préservatifs",
+                "score": 1
+            },
+            {
+                "content": "Se retirer avant de finir l'acte",
+                "score": 0
+            },
+            {
+                "content": "Ne passer que par la porte de derrière",
+                "score": 0
+            },
+            {
+                "content": "Se protéger c'est douter",
+                "score": 0
+            }
+        ]
+    },
+    {
+        "name": "tester_positif_ist",
+        "type": "question",
+        "ageRange": "adulte",
+        "content": "Comment tester si l'on est positif à une IST ?",
+        "hintLabel": "Aide",
+        "hintContent": "",
+        "responses": [
+            {
+                "content": "Aller se faire dépister en laboratoire",
+                "score": 1
+            },
+            {
+                "content": "Il suffit d'aller voir l'infermière scolaire",
+                "score": 0
+            },
+            {
+                "content": "Avec un test PCR",
+                "score": 0
+            },
+            {
+                "content": "Avec des test en ligne",
+                "score": 0
+            }
+        ]
+    },
+    {
+        "name": "ist_rependue",
+        "type": "question",
+        "ageRange": "adulte",
+        "content": "Quelle est l'IST la plus répandue ? ",
+        "hintLabel": "Aide",
+        "hintContent": "",
+        "responses": [
+            {
+                "content": "Chlamydiae",
+                "score": 1
+            },
+            {
+                "content": "Syphilis",
+                "score": 0
+            },
+            {
+                "content": "L'infection par les papillomavirus humain",
+                "score": 0
+            },
+            {
+                "content": "L'hépatite B",
+                "score": 0
+            }
+        ]
+    },
+    {
+        "name": "attraper_ist",
+        "type": "question",
+        "ageRange": "adulte",
+        "content": "Comment peut-on attraper une IST ?",
+        "hintLabel": "Aide",
+        "hintContent": "",
+        "responses": [
+            {
+                "content": "Rapport sexuel non protégé / fellation",
+                "score": 1
+            },
+            {
+                "content": "En buvant dans la même gourde que son voisin",
+                "score": 0
+            },
+            {
+                "content": "C'est fourbe, un high-five suffirait !",
+                "score": 0
+            },
+            {
+                "content": "Il faut avoir un rapport sexuel non protégé avec une personne de moins d'un mètre soixante-dix, un soir de pleine lune, en portant du latex mais seulement en été car les virus n'aiment pas le froid",
+                "score": 0
+            }
+        ]
+    },
+    {
+        "name": "ist_lesion",
+        "type": "question",
+        "ageRange": "adulte",
+        "content": "Quelle IST est souvent associé à des lésions de la peau et des muqueuse de la personne infectée ?",
+        "hintLabel": "Aide",
+        "hintContent": "",
+        "responses": [
+            {
+                "content": "Chlamydiae",
+                "score": 0
+            },
+            {
+                "content": "Syphilis",
+                "score": 1
+            },
+            {
+                "content": "L'infection par les papillomavirus humain",
+                "score": 0
+            },
+            {
+                "content": "L'hépatite B",
+                "score": 0
+            }
+        ]
+    },
+    {
+        "name": "soigner_ist",
+        "type": "question",
+        "ageRange": "adulte",
+        "content": "On peut soigner les IST, Vrai ou Faux ?",
+        "hintLabel": "Aide",
+        "hintContent": "",
+        "responses": [
+            {
+                "content": "Vrai",
+                "score": 1
+            },
+            {
+                "content": "Faux",
+                "score": 0
+            }
+        ]
+    },
+    {
+        "name": "vaccin_papillomavirus",
+        "type": "question",
+        "ageRange": "adulte",
+        "content": "A qui est destiné le vaccin pour le papillomavirus (infection du col de l'utérus) ?",
+        "hintLabel": "Aide",
+        "hintContent": "",
+        "responses": [
+            {
+                "content": "Aux jeunes femmes et aux jeunes hommes, les symptômes ne touchent que les femmes, mais les hommes peuvent le transmettre, il est ainsi reconseillé à tout le monde de se vacciner.",
+                "score": 1
+            },
+            {
+                "content": "Aux adolescentes / jeunes femmes, les hommes n'ont pas d'utérus",
+                "score": 0
+            },
+            {
+                "content": "Aux adolescents / jeunes hommes, car c'est eux qui en sont porteur et qui le distribue",
+                "score": 0
+            },
+            {
+                "content": "Aux enfants, Plus ils sont jeunes mieux c'est",
                 "score": 0
             }
         ]
