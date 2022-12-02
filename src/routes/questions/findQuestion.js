5c18eac5482c32db9f558b8b1844aef4f81e36e7
Xen0Xys <red57101@gmail.com>
2022-12-02 03:40:33 +0100

    :bug: Fix errors when no question found

--- src/routes/questions/findQuestion.js
+++ src/routes/questions/findQuestion.js
@@ -5,6 +5,8 @@ module.exports = (app) => {
         const ageRange = req.params.ageRange;
         models.Question.findAll({where: {ageRange}}).then(question => {
             const count = question.length;
+            if(count === 0)
+                return res.status(404).json({message: "No question found for this age range"});
             const random = Math.floor(Math.random() * count);
             models.Response.findAll({where: {questionId: question[random].id}}).then(responses => {
                 const questionWithResponses = {
--- src/routes/questions/getQuestion.js
+++ src/routes/questions/getQuestion.js
@@ -4,6 +4,8 @@ module.exports = (app) => {
     app.get('/api/questions', (req, res) => {
         models.Question.findAll().then(question => {
             const count = question.length;
+            if(count === 0)
+                res.status(404).json({message: "No question found"});
             const random = Math.floor(Math.random() * count);
             models.Response.findAll({where: {questionId: question[random].id}}).then(responses => {
                 const questionWithResponses = {
