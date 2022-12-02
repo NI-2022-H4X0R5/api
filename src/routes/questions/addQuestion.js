4c0735704ca2dd5aec4dcefc2eee6f2f19b730f7
Xen0Xys <red57101@gmail.com>
2022-12-02 06:02:10 +0100

    :goal_net: Add error catching to prevent API crash

--- src/routes/questions/addQuestion.js
+++ src/routes/questions/addQuestion.js
@@ -1,7 +1,13 @@
 const {models} = require('../../database/sequelize');
 
 module.exports = (app) => {
-    app.post('/api/questions', (req, res) => {
+    app.post('/api/questions', async (req, res) => {
+        if(!req.body.question.name)
+            return res.status(400).json({message: "Missing question name"});
+        const questionCheck = await models.Question.findOne({where: {name: req.body.question.name}})
+        if(!questionCheck) {
+            return res.status(400).json({message: "Question name already exists"});
+        }
         models.Question.create(req.body.question).then(question => {
             const responses = req.body.responses;
             for(let response of responses){
@@ -12,7 +18,8 @@ module.exports = (app) => {
                 });
             }
             res.status(201).json(question);
+        }).catch(err => {
+            res.status(400).json(err);
         });
-
     });
 }
--- src/routes/user/addUser.js
+++ src/routes/user/addUser.js
@@ -4,9 +4,11 @@ const {encrypt} = require('../../tools/encryption');
 module.exports = (app) => {
     app.post('/api/users', async (req, res) => {
         const {username, password} = req.body;
-        if (!username || !password) {
+        if (!username || !password)
             return res.status(400).json({message: "Missing username or password"});
-        }
+        const userCheck = await models.User.findOne({where: {username: req.body.username}})
+        if(!userCheck)
+            return res.status(400).json({message: "Username already exists"});
         const encryptedPassword = await encrypt(password);
         const user = await models.User.create({username, password: encryptedPassword});
         const jsonUser = user.toJSON();
