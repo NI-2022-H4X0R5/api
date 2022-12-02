e9afd661e6fef75d2adb9f1c303985ddc67cb1a8
Xen0Xys <red57101@gmail.com>
2022-12-02 05:23:38 +0100

    :ambulance: Fix database name

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -14,7 +14,7 @@ if(type === "sqlite"){
         logging: logging === "true"
     });
 }else{
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
         dialect: process.env.DB_TYPE,
         logging: process.env.DB_LOGGING === "true"
